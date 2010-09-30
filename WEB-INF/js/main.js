4f66631f2368d54ce54820bc38881751cf59fc2b
Georgi Lambov <dev@uvwebs.com>
2010-09-30 14:12:18 +0300

    Added: SEO css layout for the design

--- WEB-INF/js/main.js
+++ WEB-INF/js/main.js
@@ -1,1 +1,28 @@
-@CHARSET "UTF-8";
+if (typeof APPLICATION == "undefined" || !APPLICATION) {
+  var APPLICATION = {};
+}
+
+if (typeof APPLICATION.position == "undefined" || !APPLICATION.position) {
+	APPLICATION.position = {};
+}
+
+// reorder div container position
+APPLICATION.position.firstContainerBellowSecond = function(first, second) {
+	
+    var init = function() {
+    	var topElement = document.getElementById(first);
+    	var bottomElement = document.getElementById(second);
+    	
+    	swapNodes(topElement, bottomElement);
+    };
+    
+    var swapNodes = function(topNode, bottomNode) {
+    	topNode.parentNode.replaceChild(topNode, bottomNode);
+        topNode.parentNode.insertBefore(bottomNode, topNode);     	
+    };
+
+    return {
+        init: init
+    };
+    
+};
